608bddf2204ef3da9a11e6296b2595d4ca4b3cb2
Radiant Juan <radiant@ordermate.com.au>
2017-07-11 17:24:34 +0800

    sub menu grid layout

--- app/assets/scripts/master.js
+++ app/assets/scripts/master.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
+	$('.main-menu-nav-group-items').mouseover(function(){
+		$(this).find('.main-sub-menu-nav-group').slideDown('fast');
+	});
 
+	$('.main-menu-nav-group-items').mouseleave(function(){
+		$(this).find('.main-sub-menu-nav-group').slideUp('fast');
+	});
 });
